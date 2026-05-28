c86b32870a349726bb811418fe8a3ef3a8af515d
Mat Booth <mat.booth@gmail.com>
2026-05-28 08:53:48 +0100

    Fix incorrect copyright headers

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """
 Copyright (c) 2026 Mat Booth.
 
-This file is part of the Settlers of EMF app for the Tildagon
-(see https://github.com/mbooth101/emf-settlers).
+This file is part of the Speedo app for the Tildagon
+(see https://github.com/mbooth101/emf-speedometer).
 
 License: MIT
 """
